77e79a0f9541782f8c56b3a7dbcd6c4e02d8754a
Dylan Kamauoha <dylantkamauoha@gmail.com>
2018-07-02 15:21:27 -0600

    Finished Question 6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -2,17 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Todo from './components/Todo';
+
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      tasks: [],
+      task: ''
+    }
+    this.addToTasks = this.addToTasks.bind(this);
+  }
+
+  handleChange (val) {
+    this.setState({task: val})
+  }
+
+  addToTasks() {
+    // let arr = [...this.state.tasks];
+    // arr.push(this.state.task)
+    // this.setState({tasks: arr})
+    this.setState({tasks: [...this.state.tasks, this.state.task], task: ''})
+    
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do-list:</h1>
+        <input onChange={(e) => this.handleChange(e.target.value)} type="text"/>
+        <button onClick={() => this.addToTasks()}>Add</button>
+        <Todo tasks={this.state.tasks}/>
       </div>
     );
   }
